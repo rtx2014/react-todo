6f7fb2a2dbf73e9ea69aff882af990a10ff11fbc
Vladimir Kim <rtx2014@gmail.com>
2016-10-29 20:22:12 +0300

    Add test suite for list component

---
+++ app/components/TodoList.jsx
@@ -0,0 +1,24 @@
+var React = require('react');
+var Todo = require('Todo');
+
+var TodoList = React.createClass({
+  render() {
+    var renderTodos = () => {
+      var {todos} = this.props;
+
+      return todos.map((todo) => {
+        return (
+          <Todo key={todo.id} {...todo}/>
+        );
+      });
+    };
+
+    return (
+      <div>
+        {renderTodos()}
+      </div>
+    )
+  }
+});
+
+module.exports = TodoList;
