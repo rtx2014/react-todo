c68e7bfb0ba34ad85a3480afa2ee50b4231d69b4
Vladimir Kim <rtx2014@gmail.com>
2016-10-31 00:34:58 +0300

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -14,9 +14,33 @@ module.exports = {
     try {
       todos = JSON.parse(stringTodos);
     } catch (e) {
-      
+
     };
 
     return $.isArray(todos) ? todos : [];
+  },
+  filterTodos(todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+    filteredTodos.sort((a, b) => {
+      if (!a.completed && b.completed) {
+        return -1;
+      } else if (a.completed && !b.completed) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    if (searchText.length > 0) {
+      filteredTodos = filteredTodos.filter((todo) => {
+        return todo.text.toLowerCase().indexOf(searchText) >= 0;
+      });
+    }
+
+    return filteredTodos;
   }
 };
