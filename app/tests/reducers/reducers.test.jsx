d9c22cc8561cc1741129423c6704e7f1a527885a
Vladimir Kim <rtx2014@gmail.com>
2016-11-05 20:09:47 +0300

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -4,6 +4,7 @@ var df = require('deep-freeze-strict');
 var reducers = require('reducers');
 
 describe('Reducers', () => {
+
   describe('searchTextReducer', () => {
     it('should set searchText', () => {
       var action = {
@@ -15,6 +16,7 @@ describe('Reducers', () => {
       expect(res).toEqual(action.searchText);
     });
   });
+
   describe('showCompletedReducer', () =>{
     it('should toggle showCompleted', () => {
       var action = {
@@ -23,6 +25,38 @@ describe('Reducers', () => {
       var res = reducers.showCompletedReducer(df(false), df(action));
 
       expect(res).toEqual(true);
+    });
+  })
+
+  describe('todosReducer', () =>{
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Fuck teen'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle todo', () => {
+      var todos = [{
+        id: '123',
+        text: 'Do something',
+        completed: true,
+        createdAt: 123,
+        completedAt: 125
+      }];
+
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: '123'
+      };
+      var res = reducers.todosReducer(df(todos), df(action));
+
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
     });
   })
 });
